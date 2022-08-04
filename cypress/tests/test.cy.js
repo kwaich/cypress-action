4af2cacf3ef8c19cf403064b18ad446ab71f42e2
Wai Cheng <waicheng.kong@foodpanda.com>
2022-08-04 16:41:55 +0800

    update

--- cypress/tests/test.cy.js
+++ cypress/tests/test.cy.js
@@ -5,14 +5,4 @@ describe('hello world', () => {
     cy.visit('https://www.helloworld.org/')
     cy.get('h1').should('have.text', "Hello World")
   })
-
-  it('visit site on host', () => {
-    cy.visit('/hello')
-    cy.request({
-      method: 'GET',
-      url: '/hello',
-    }).then(({ status }) => {
-      expect(status).to.eq(200)
-    })
-  });
 })
