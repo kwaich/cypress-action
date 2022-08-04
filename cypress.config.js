adfafde12667bb7caf9994f7bfb2282de19d11b5
Wai Cheng <waicheng.kong@foodpanda.com>
2022-08-04 16:19:15 +0800

    update

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,10 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/tests/**/*.cy.{js,jsx,ts,tsx}',
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'results/test-output-[hash].xml'
+    }
   },
 });
--- cypress/tests/test.cy.js
+++ cypress/tests/test.cy.js
@@ -5,4 +5,14 @@ describe('hello world', () => {
     cy.visit('https://www.helloworld.org/')
     cy.get('h1').should('have.text', "Hello World")
   })
+
+  it('visit site on host', () => {
+    cy.visit('/hello')
+    cy.request({
+      method: 'GET',
+      url: '/hello',
+    }).then(({ status }) => {
+      expect(status).to.eq(200)
+    })
+  });
 })
